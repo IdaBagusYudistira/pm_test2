ea25ae914f1db0d6096107a3e5d5114d6c8fffe4
unknown <diravi1814@gmail.com>
2024-11-05 15:46:31 +0900

    Edited Hello.java

--- Hello.java
+++ Hello.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class Hello {
     private String name;
     
-    public Hello (String name) {
+    public Hello(String name) {
         this.name = name;
     }
     
     public static void main (String[] args) {
-        Hello hello = new Hello("Yudis");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Hey! Insert your name here: ");
+        Hello hello = new Hello(sc.nextLine());
         
         System.out.printf("Hello, %s!", hello.name);
     }
