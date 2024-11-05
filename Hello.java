74f9ba5516cb2e43e9734f19259555ad4a3a3893
IdaBagusYudistira <6182101046@student.unpar.ac.id>
2024-11-05 15:39:53 +0900

    Update Hello.java
    Added name.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
+    private String name;
+    
+    public Hello (String name) {
+        this.name = name;
+    }
+    
     public static void main (String[] args) {
-        System.out.println("Hello world!"));
+        Hello hello = new Hello("Yudis");
+        
+        System.out.printf("Hello, %s!", hello.name);
     }
 }
