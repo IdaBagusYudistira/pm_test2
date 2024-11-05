bd2119f8454aa92b132666c86672179ab8598bbe
unknown <diravi1814@gmail.com>
2024-11-05 15:47:47 +0900

    Edited Hello.java

--- Hello.java
+++ Hello.java
@@ -10,7 +10,7 @@ public class Hello {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Hey! Insert your name here: ");
+        System.out.print("Hey! Please insert your name here: ");
         Hello hello = new Hello(sc.nextLine());
         
         System.out.printf("Hello, %s!", hello.name);
